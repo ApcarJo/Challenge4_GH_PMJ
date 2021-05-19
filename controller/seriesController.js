3ed1fd6405b9f74d2e9281b664953d29dcf693e0
Miguel Torres <migueltm.sp@gmail.com>
2021-05-19 13:54:37 +0200

    Completed Obtener series por id

--- controller/seriesController.js
+++ controller/seriesController.js
@@ -3,7 +3,11 @@ const axios = require("axios");
 class Serie{
 
     // Métodos controladores
-
+    async searchById(id){
+        let res = await axios.get(`https://api.themoviedb.org/3/tv/${id}?api_key=210d6a5dd3f16419ce349c9f1b200d6d&language=en-US`);
+        return res.data;
+     }
+     
 
 }
 
--- routes/seriesRouter.js
+++ routes/seriesRouter.js
@@ -2,8 +2,17 @@ const router = require("express").Router();
 const seriesController = require("../controllers/seriesController");
 
 // CRUD
+// GET
 
-
-
+router.get("/search/:id", async (req,res) =>{
+    try{
+        let id = req.params.id;
+        res.json(await seriesController.searchById(id));
+    }catch (err){
+        return res.status(500).json({
+            message: err.message
+    });
+    }
+});
 
 module.exports = router;

2626a69799e5e68fd83e6d9aa73e3c529bd05816
ApcarJo <hansokaz@gmail.com>
2021-05-19 16:15:43 +0200

    Added top rated movies methods and controller

--- controller/moviesController.js
+++ controller/moviesController.js
@@ -3,6 +3,10 @@ const axios = require("axios");
 class Pelicula{
 
     // Métodos controladores
+    async findTopRated(){
+        let res = await axios.get('https://api.themoviedb.org/3/genre/movie/list?api_key=210d6a5dd3f16419ce349c9f1b200d6d&language=en-US');
+        return res.data;
+    }
 
     
 
--- routes/moviesRouter.js
+++ routes/moviesRouter.js
@@ -3,6 +3,15 @@ const moviesController = require("../controllers/moviesController");
 
 // CRUD
 
+router.get('/', async (req, res) => {
+    try {
+        res.json(await moviesController.findTopRated());
+    }catch (err) {
+        return res.status(500).json({
+        message: err.message
+        });
+    }
+});
 
 
 
